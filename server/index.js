7484903ed0e37c73cb98976f12d0c9a90cd932f2
nazmul-devs <nazmulhosenm668@gmail.com>
2025-08-23 11:28:50 +0600

    'commit'

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const grpc = require("grpc");
+
+function main() {
+  const server = new grpc.Server();
+
+  server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+
+  server.start();
+
+  console.log("Server running...");
+}
+
+// this is main fuc call here
+
+main();
